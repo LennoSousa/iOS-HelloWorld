4ed2594f586a391c3ceb1f224f82f42c2b25b23c
Lenno Sousa <lenno_sousa@hotmail.com>
2021-06-15 10:58:26 -0300

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Lenno Sousa on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
